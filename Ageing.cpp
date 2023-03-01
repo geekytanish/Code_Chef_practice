16cb37e810301bc1897a7ea2cec514e8f79d4e04
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-03-01 21:51:59 +0530

    Create Ageing.cpp

---
+++ Ageing.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	for(int i=0; i<t; i=i+1){
+	    int x;
+	    cin>>x;
+	    
+	    cout<<x-10<<endl;;
+	}
+	return 0;
+}
